8eba7b2759e82a30c92c8f55689183f3578dff29
BertramYe <bertramyerik@gmail>
2025-10-10 01:57:48 +0800

    add the part of the notes of the borrow and reference

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::string;
-
-
-
-
-
 
 fn main() {   
- 
+    let mut x = 5;
+    let p: *mut i32 = &mut x;  // 可变裸指针
+    unsafe {
+        *p = 10;  // 通过裸指针修改数据
+    };
+    unsafe  {
+        println!("the value of the p points to: {}",*p); // 解引用裸指针，打印它指向的值
+    }
 }
 
 
